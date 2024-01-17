08c049e2b9bca871282c0c24e4d98f324d07bba5
yujin <108935743+yujin0708@users.noreply.github.com>
2024-01-17 16:51:35 +0900

    feat: 음성 채널 단체 이동 및 그룹 취소 시 음성 채널 삭제 구현

--- src/discord/discord.service.ts
+++ src/discord/discord.service.ts
@@ -1,5 +1,10 @@
 import { Injectable, NotFoundException, OnModuleInit } from "@nestjs/common";
-import { ChannelType, Client, VoiceChannel } from "discord.js";
+import {
+    ChannelType,
+    Client,
+    VoiceBasedChannel,
+    VoiceChannel,
+} from "discord.js";
 import { ConfigService } from "@nestjs/config";
 import { GroupService } from "src/group/group.service";
 import { UserService } from "src/user/user.service";
@@ -9,6 +14,7 @@ export class DiscordService implements OnModuleInit {
     private readonly client: Client<boolean> = new Client({
         intents: ["Guilds", "GuildVoiceStates", "GuildMembers"],
     });
+    private groupChannelMap = new Map<string, string>();
 
     constructor(
         private readonly configService: ConfigService,
@@ -17,22 +23,67 @@ export class DiscordService implements OnModuleInit {
     ) {}
 
     async onModuleInit(): Promise<void> {
-        const botToken: string =
-            this.configService.get<string>("DISCORD_BOT_TOKEN");
+        const botToken = this.configService.get<string>("DISCORD_BOT_TOKEN");
         await this.client.login(botToken);
+
+        const lobbyChannelId = this.configService.get<string>(
+            "DISCORD_LOBBY_CHANNEL_ID",
+        );
 
         this.client.on("voiceStateUpdate", async (oldState, newState) => {
             if (oldState.channelId && !newState.channelId) {
                 const channel = oldState.channel;
-                if (channel && channel.members.size === 0) {
-                    try {
-                        await channel.delete();
-                    } catch (error) {
-                        console.error("음성 채널 삭제 실패:", error);
+
+                if (channel) {
+                    const shouldDelete = this.shouldDeleteChannel(
+                        channel,
+                        lobbyChannelId,
+                    );
+                    if (shouldDelete) {
+                        console.log("채널 삭제 `조건 충족:", channel.id);
+                        await this.deleteChannel(channel);
                     }
                 }
             }
         });
+    }
+
+    // 채널 삭제 조건 검증
+    private shouldDeleteChannel(
+        channel: VoiceBasedChannel,
+        lobbyChannelId: string,
+    ): boolean {
+        return channel.id !== lobbyChannelId && this.isChannelEmpty(channel);
+    }
+
+    // 채널 유저 검증
+    private isChannelEmpty(channel: VoiceBasedChannel): boolean {
+        return !channel.guild.members.cache.some(
+            (member) => member.voice.channelId === channel.id,
+        );
+    }
+
+    // 채널 삭제
+    private async deleteChannel(channel: VoiceBasedChannel): Promise<void> {
+        try {
+            await channel.delete();
+            console.log(`채널 삭제 성공: ${channel.id}`);
+        } catch (error) {
+            console.error(`채널 삭제 중 오류 발생: ${channel.id}`, error);
+        }
+    }
+
+    // 그룹 취소 시 음성 채널 삭제
+    async deleteVoiceChannelForGroup(groupId: string): Promise<void> {
+        const channelId = this.groupChannelMap.get(groupId);
+
+        if (channelId) {
+            const channel = this.client.channels.cache.get(channelId);
+            if (channel) {
+                await this.deleteChannel(channel as VoiceBasedChannel);
+            }
+            this.groupChannelMap.delete(groupId);
+        }
     }
 
     // 디스코드 음성 채널 생성 및 역할 연결
@@ -72,31 +123,38 @@ export class DiscordService implements OnModuleInit {
                 },
             ],
         });
+        this.groupChannelMap.set(groupId, voiceChannel.id);
 
         return { voiceChannel, role };
     }
 
-    // 디스코드 음성 채널에 유저 추가
+    // 채널 및 역할 추가
     async assignRoleAndMoveToChannel(
-        discordId: string,
+        discordIds: string[],
         guildId: string,
         roleId: string,
         channelId: string,
     ): Promise<void> {
         const guild = this.client.guilds.cache.get(guildId);
-        const member = await guild.members.fetch(discordId);
         const channel = guild.channels.cache.get(channelId) as VoiceChannel;
 
-        if (!member || !channel) {
+        if (!channel) {
             throw new NotFoundException(
                 "해당 유저 또는 채널을 찾을 수 없습니다.",
             );
         }
 
-        await member.roles.add(roleId);
-        await member.voice.setChannel(channel);
+        for (const discordId of discordIds) {
+            const member = await guild.members.fetch(discordId);
+
+            if (member) {
+                await member.roles.add(roleId);
+                await member.voice.setChannel(channel);
+            }
+        }
     }
 
+    // 음성 채널 이동시킬 유저 세팅
     async setupUserVoiceChannel(
         guildId: string,
         discordId: string,
@@ -106,11 +164,30 @@ export class DiscordService implements OnModuleInit {
             discordId,
         );
 
+        const user = await this.userService.findOneByDiscordId(discordId);
+
+        const groupId: string = await this.groupService.findGroupIdByOwner(
+            user.id,
+        );
+
+        const groupState = await this.groupService.findGroupStateById(groupId);
+        const positions = ["mid", "adc", "sup", "top", "jg"];
+
+        const userIds = positions
+            .filter((position) => groupState[position].isActive)
+            .map((position) => groupState[position].userId);
+
+        const discordIds = await Promise.all(
+            userIds.map((userId) =>
+                this.userService.findDiscordIdByUserId(userId),
+            ),
+        );
+
         const voiceChannelId: string = voiceChannel.id;
         const roleId: string = role.id;
 
         await this.assignRoleAndMoveToChannel(
-            discordId,
+            discordIds,
             guildId,
             roleId,
             voiceChannelId,
