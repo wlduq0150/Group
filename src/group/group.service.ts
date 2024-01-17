7cc5515d1f79b2c88c7f544e726165cdd7cb1fed
wlduq0150 <wlduq0150@gmail.com>
2024-01-17 10:34:22 +0900

    그룹 동시서 처리 완료

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -6,19 +6,24 @@ import { GroupState } from "./interface/group-state.interface";
 import { Position } from "./type/position.type";
 import { checkIsUserPositionSelect } from "./function/check-position-select.function";
 import { WsException } from "./exception/ws-exception.exception";
-import { Server, Socket } from "socket.io";
 import { RedisService } from "src/redis/redis.service";
+import Redlock from "redlock";
 
 @Injectable()
 export class GroupService {
-    constructor(private readonly redisService: RedisService) {}
+    private redlock: Redlock;
+
+    constructor(private readonly redisService: RedisService) {
+        this.clear();
+
+        this.redlock = new Redlock([redisService.getRedisClient()], {
+            retryCount: 3,
+            retryDelay: 300,
+        });
+    }
 
     async clear() {
         await this.redisService.clear();
-    }
-
-    async getAll() {
-        return await this.redisService.getAll();
     }
 
     async createGroup(groupId: string, createGroupDto: CreateGroupDto) {
@@ -27,13 +32,21 @@ export class GroupService {
         const groupInfoKey = `group-${groupId}`;
         const groupStateKey = `group-${groupId}-state`;
 
-        const group: Group = { name, mode, mic, owner };
+        const group: Group = { name, mode, mic, owner, open: true };
         const groupState = initGroupState(position);
 
         await this.redisService.set(groupInfoKey, JSON.stringify(group));
         await this.redisService.set(groupStateKey, JSON.stringify(groupState));
 
         return group;
+    }
+
+    // 모든 그룹 id 반환
+    async findAllGroup() {
+        const redisClient = this.redisService.getRedisClient();
+        let keys = await redisClient.keys("group-*#");
+        keys = keys.map((key) => key.replace("group-", ""));
+        return keys;
     }
 
     // 유저 아이디를 통해 해당 유저가 방장인 그룹Id를 반환(없을 경우 null 반환)
@@ -90,266 +103,114 @@ export class GroupService {
 
     // userId 지우기
     async joinGroup(groupId: string) {
+        const groupInfoKey = `group-${groupId}`;
         const groupStateKey = `group-${groupId}-state`;
+        const groupStateLockkey = `Lock:group:${groupId}:state`;
 
+        const lock = await this.redlock.acquire([groupStateLockkey], 1000);
+
+        const groupInfo = await this.findGroupInfoById(groupId);
         const groupState = await this.findGroupStateById(groupId);
 
-        if (groupState.currentUser >= groupState.totalUser) {
-            throw new WsException(
-                "그룹 인원수 제한으로 인해 참여할 수 없습니다.",
+        try {
+            if (!groupInfo.open) {
+                throw new Error("그룹이 현재 비공개 상태입니다.");
+            }
+
+            if (groupState.currentUser >= groupState.totalUser) {
+                throw new Error(
+                    "그룹 인원수 제한으로 인해 참여할 수 없습니다.",
+                );
+            }
+
+            groupState.currentUser += 1;
+
+            await this.redisService.set(
+                groupStateKey,
+                JSON.stringify(groupState),
             );
+        } catch (e) {
+            throw new WsException(e.message);
+        } finally {
+            await lock.release();
         }
-
-        groupState.currentUser += 1;
-
-        this.redisService.set(groupStateKey, JSON.stringify(groupState));
 
         return groupState;
     }
 
     async leaveGroup(groupId: string, userId: number) {
-        const groupInfoKey = `group-${groupId}`;
         const groupStateKey = `group-${groupId}-state`;
+        const groupStateLockkey = `Lock:group:${groupId}:state`;
 
-        const groupState = await this.findGroupStateById(groupId);
+        const lock = await this.redlock.acquire([groupStateLockkey], 1000);
 
-        // 현재 포지션을 선택한 상태라면 포지션 해제
-        const pos = checkIsUserPositionSelect(groupState, userId);
-        if (pos !== "none") {
-            await this.deselectPosition(groupId, userId, pos);
-        }
+        let groupState = await this.findGroupStateById(groupId);
 
-        // 유저 나가기
-        groupState.currentUser -= 1;
+        try {
+            // 현재 포지션을 선택한 상태라면 포지션 해제
+            const pos = checkIsUserPositionSelect(groupState, userId);
+            if (pos !== "none") {
+                groupState = await this.deselectPosition(groupId, userId, pos);
+            }
 
-        // 해당 그룹의 지속 여부
-        const isGroupEmpty = groupState.currentUser > 0 ? false : true;
+            // 유저 나가기
+            groupState.currentUser -= 1;
 
-        if (isGroupEmpty) {
-            await this.redisService.del(groupInfoKey);
-            await this.redisService.del(groupStateKey);
-            return null;
-        } else {
-            await this.redisService.set(
-                groupStateKey,
-                JSON.stringify(groupState),
-            );
-            return groupState;
+            // 해당 그룹의 지속 여부
+            const isGroupEmpty = groupState.currentUser > 0 ? false : true;
+
+            if (isGroupEmpty) {
+                await this.removeGroup(groupId);
+                return null;
+            } else {
+                await this.redisService.set(
+                    groupStateKey,
+                    JSON.stringify(groupState),
+                );
+                return groupState;
+            }
+        } catch (e) {
+            throw new WsException(e.message);
+        } finally {
+            await lock.release();
         }
     }
 
-    // 레디스의 multi와 watch를 통해 트랜잭션과 락 구현
-    // async selectPosition(groupId: string, userId: number, position: Position) {
-    //     const groupStateKey = `group-${groupId}-state`;
-
-    //     const redisClient = this.redisService.getRedisClient();
-
-    //     redisClient.watch(groupStateKey);
-
-    //     const transaction = redisClient.multi();
-
-    //     let groupState: GroupState;
-
-    //     await transaction
-    //         .get(groupStateKey, async (err, result) => {
-    //             groupState = await this.findGroupStateById(groupId);
-    //             console.log(groupState);
-    //             groupState[position].userId = userId;
-    //         })
-    //         .set(groupStateKey, JSON.stringify(groupState))
-    //         .exec((err, data: any) => {
-    //             groupState = JSON.parse(data[0][1]);
-
-    //             try {
-    //                 if (err) {
-    //                     throw new WsException(err.message);
-    //                 }
-
-    //                 if (!groupState) {
-    //                     throw new WsException("그룹이 존재하지 않습니다.");
-    //                 }
-
-    //                 if (!groupState[position].isActive) {
-    //                     throw new WsException(
-    //                         "해당 포지션은 선택할 수 없습니다.",
-    //                     );
-    //                 }
-
-    //                 if (groupState[position].userId !== null) {
-    //                     throw new WsException(
-    //                         "다른 사용자가 이미 해당 포지션을 선택했습니다.",
-    //                     );
-    //                 }
-
-    //                 if (
-    //                     checkIsUserPositionSelect(groupState, userId) !== "none"
-    //                 ) {
-    //                     throw new WsException("이미 포지션을 선택했습니다.");
-    //                 }
-    //             } catch (e) {
-    //                 transaction.discard();
-    //                 throw e;
-    //             }
-    //         });
-
-    //     return groupState;
-    // }
-
-    // async selectPosition(groupId: string, userId: number, position: Position) {
-    //     const groupStateKey = `group-${groupId}-state`;
-    //     const groupStateLockKey = `group-${groupId}-state-lock`;
-
-    //     const groupStateLock = await this.redisService.get(groupStateLockKey);
-    //     if (groupStateLock === "1") {
-    //         throw new WsException("다른 유저가 선택중입니다.");
-    //     }
-
-    //     console.log(position, "잠금해제");
-
-    //     const groupState = await this.findGroupStateById(groupId);
-    //     await this.redisService.set(groupStateLockKey, "1");
-    //     console.log(groupState, position);
-
-    //     if (!groupState) {
-    //         throw new WsException("그룹이 존재하지 않습니다.");
-    //     }
-
-    //     if (!groupState[position].isActive) {
-    //         throw new WsException("해당 포지션은 선택할 수 없습니다.");
-    //     }
-
-    //     if (groupState[position].userId !== null) {
-    //         throw new WsException(
-    //             "다른 사용자가 이미 해당 포지션을 선택했습니다.",
-    //         );
-    //     }
-
-    //     if (checkIsUserPositionSelect(groupState, userId) !== "none") {
-    //         throw new WsException("이미 포지션을 선택했습니다.");
-    //     }
-
-    //     groupState[position].userId = userId;
-    //     await this.redisService.set(groupStateKey, groupState);
-
-    //     await this.redisService.set(groupStateLockKey, "0");
-
-    //     console.log(position, "완료");
-
-    //     return groupState;
-    // }
-
-    // async selectPosition(groupId: string, userId: number, position: Position) {
-    //     const groupStateKey = `group-${groupId}-state`;
-    //     const groupStateLockKey = `group-${groupId}-state-lock`;
-
-    //     const redisClient = this.redisService.getRedisClient();
-    //     const transaction = redisClient.multi();
-
-    //     let groupState: GroupState;
-    //     let exception: WsException;
-
-    //     await transaction
-    //         .get(groupStateLockKey)
-    //         .exec(async (err, data: any) => {
-    //             const groupStateLock = JSON.parse(data[0][1]);
-
-    //             if (groupStateLock === "1") {
-    //                 exception = new WsException("다른 유저가 선택중입니다.");
-    //             }
-
-    //             await this.redisService.set(groupStateLockKey, "1");
-    //             console.log(position, "잠금");
-
-    //             groupState = await this.findGroupStateById(groupId);
-    //             console.log(groupState, position);
-
-    //             if (!groupState) {
-    //                 exception = new WsException("그룹이 존재하지 않습니다.");
-    //             }
-
-    //             if (!groupState[position].isActive) {
-    //                 exception = new WsException(
-    //                     "해당 포지션은 선택할 수 없습니다.",
-    //                 );
-    //             }
-
-    //             if (groupState[position].userId !== null) {
-    //                 exception = new WsException(
-    //                     "다른 사용자가 이미 해당 포지션을 선택했습니다.",
-    //                 );
-    //             }
-
-    //             if (checkIsUserPositionSelect(groupState, userId) !== "none") {
-    //                 exception = new WsException("이미 포지션을 선택했습니다.");
-    //             }
-
-    //             groupState[position].userId = userId;
-    //             await this.redisService.set(groupStateKey, groupState);
-
-    //             await this.redisService.set(groupStateLockKey, "0");
-    //         });
-    //     if (exception) {
-    //         throw exception;
-    //     }
-    //     console.log(position, "완료");
-
-    //     return groupState;
-    // }
-
     async selectPosition(groupId: string, userId: number, position: Position) {
         const groupStateKey = `group-${groupId}-state`;
-        const groupStateErrorKey = `group-${groupId}-state-error`;
+        const groupStateLockkey = `Lock:group:${groupId}:state`;
 
         const redisClient = this.redisService.getRedisClient();
 
-        let wsException: WsException;
-
-        redisClient.defineCommand("selectPosition", {
-            numberOfKeys: 2,
-            lua: `
-                local data = redis.call('GET', KEYS[1])
-                local groupState = cjson.decode(data) or {}
-
-                local position = ARGV[1]
-                local userId = tonumber(ARGV[2])
-
-                if not groupState[position] then
-                    redis.call('SET', KEYS[2], cjson.encode(groupState[position]))
-                    return
-                end
-
-                groupState[position].userId = userId
-                redis.call('SET', KEYS[1], cjson.encode(groupState))
-
-                return groupState
-            `,
-        });
-
-        redisClient.multi({ pipeline: false });
-        //@ts-ignore
-        redisClient.selectPosition(
-            groupStateKey,
-            groupStateErrorKey,
-            position,
-            userId,
-        );
-        redisClient.get(groupStateErrorKey);
-        await redisClient.exec((err, results: any) => {
-            console.log("에러: ", err);
-            console.log("결과: ", results);
-
-            const errorMessage: string = results[1][1];
-            if (errorMessage !== null) {
-                wsException = new WsException(errorMessage);
-            }
-        });
-
-        if (wsException) {
-            throw wsException;
-        }
+        const lock = await this.redlock.acquire([groupStateLockkey], 1000);
 
         const groupState = await this.findGroupStateById(groupId);
-        console.log(groupState, "완료");
+
+        try {
+            if (!groupState[position].isActive) {
+                throw new Error("해당 포지션은 선택할 수 없습니다.");
+            }
+
+            if (groupState[position].userId !== null) {
+                throw new Error("다른 사용자가 해당 포지션을 선택했습니다.");
+            }
+
+            if (checkIsUserPositionSelect(groupState, userId) !== "none") {
+                throw new Error("이미 포지션을 선택했습니다.");
+            }
+
+            groupState[position].userId = userId;
+
+            redisClient
+                .multi()
+                .set(groupStateKey, JSON.stringify(groupState))
+                .exec();
+        } catch (e) {
+            throw new WsException(e.message);
+        } finally {
+            // 락 해제
+            await lock.release();
+        }
 
         return groupState;
     }
@@ -368,7 +229,6 @@ export class GroupService {
         }
 
         if (groupState[position].userId !== userId) {
-            console.log(groupState);
             throw new WsException("포지션을 선택하지 않았습니다.");
         }
 
