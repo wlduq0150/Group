76e0daf5f955de8730a833337ad9e9d8840a5bfe
poohtime <pooh_time@naver.com>
2024-01-22 18:41:53 +0900

    feat 프로필 모달 그룹생성 모달 숨기기

---
+++ public/js/index.js
@@ -0,0 +1,23 @@
+const profileOpenButton = document.getElementById("profileOpenButton");
+const profileCloseButton = document.getElementById("profileCloseButton");
+const profile = document.getElementById("profileContainer");
+
+profileOpenButton.addEventListener("click", () => {
+    profile.classList.remove("hidden");
+});
+
+profileCloseButton.addEventListener("click", () => {
+    profile.classList.add("hidden");
+});
+
+const groupOpenButton = document.getElementById("groupOpenButton");
+const groupCloseButton = document.getElementById("groupCloseButton");
+const group = document.getElementById("groupContainer");
+
+groupOpenButton.addEventListener("click", () => {
+    group.classList.remove("hidden");
+});
+
+groupCloseButton.addEventListener("click", () => {
+    group.classList.add("hidden");
+});
