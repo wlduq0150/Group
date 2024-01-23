62ee15e8f28328da351ded7c7355cb6f41b53a7a
poohtime <pooh_time@naver.com>
2024-01-23 18:43:05 +0900

    Css수정

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,14 @@
-// const profileOpenButton = document.getElementById("profileOpenButton");
-// const profileCloseButton = document.getElementById("profileCloseButton");
-// const profile = document.getElementById("profileContainer");
+const profileOpenButton = document.getElementById("profileOpenButton");
+const profileCloseButton = document.getElementById("profileCloseButton");
+const profile = document.getElementById("profileContainer");
 
-// profileOpenButton.addEventListener("click", () => {
-//     profile.classList.remove("hidden");
-// });
+profileOpenButton.addEventListener("click", () => {
+    profile.classList.remove("hidden");
+});
 
-// profileCloseButton.addEventListener("click", () => {
-//     profile.classList.add("hidden");
-// });
+profileCloseButton.addEventListener("click", () => {
+    profile.classList.add("hidden");
+});
 
 const groupOpenButton = document.getElementById("groupOpenButton");
 const groupCloseButton = document.getElementById("groupCloseButton");
@@ -18,20 +18,20 @@ groupOpenButton.addEventListener("click", () => {
     group.classList.remove("hidden");
 });
 
-// groupCloseButton.addEventListener("click", () => {
-//     group.classList.add("hidden");
-// });
+groupCloseButton.addEventListener("click", () => {
+    group.classList.add("hidden");
+});
 
 // 외부 영역 클릭 시 모달 닫기
-// window.addEventListener("click", (event) => {
-//     if (event.target === profile) {
-//         profile.classList.add("hidden");
-//     }
-// });
+window.addEventListener("click", (event) => {
+    if (event.target === profile) {
+        profile.classList.add("hidden");
+    }
+});
 
-// // 외부 영역 클릭 시 모달 닫기
-// window.addEventListener("click", (event) => {
-//     if (event.target === group) {
-//         group.classList.add("hidden");
-//     }
-// });
+// 외부 영역 클릭 시 모달 닫기
+window.addEventListener("click", (event) => {
+    if (event.target === group) {
+        group.classList.add("hidden");
+    }
+});
