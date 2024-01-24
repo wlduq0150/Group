6c55f6b412f8f07c888b50cfeb7e38eb7b2e8467
wlduq0150 <wlduq0150@gmail.com>
2024-01-24 19:00:15 +0900

    프론트 친구 신청 알림 추가

--- src/notice/notice.module.ts
+++ src/notice/notice.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { NoticeController } from "./notice.controller";
 import { NoticeService } from "./notice.service";
+import { UserModule } from "src/user/user.module";
 
 @Module({
+    imports: [UserModule],
     exports: [NoticeService],
     controllers: [NoticeController],
     providers: [NoticeService],
