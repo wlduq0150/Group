662df6d3e8170e54f0c9a18d92e026947c8f940d
wlduq0150 <wlduq0150@gmail.com>
2024-01-30 18:02:36 +0900

    pm2 환경변수 실행 파일 추가

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = {
+    apps: [
+        {
+            name: "socket", // pm2로 실행한 프로세스 목록에서 이 애플리케이션의 이름으로 지정될 문자열
+            script: "./dist/main.js", // pm2로 실행될 파일 경로
+            watch: true, // 파일이 변경되면 자동으로 재실행 (true || false)
+            env: {
+                NODE_ENV: "development", // 개발환경시 적용될 설정 지정
+            },
+        },
+    ],
+};
