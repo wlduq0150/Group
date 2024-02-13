a5a494bda1bd7119082f8a8696df6a1460e25329
poohtime <pooh_time@naver.com>
2024-02-13 23:45:28 +0900

    fix: eslint 관련 문제 수정, "prettier/@typescript-eslint" has been merged into "prettier" in eslint-config-prettier 8.0.0.

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/explicit-module-boundary-types": "off",
         "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
         "prettier/prettier": ["error", { "endOfLine": "auto" }]
     },
     ignorePatterns: [".eslintrc.js"]
