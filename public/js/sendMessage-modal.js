6c08a37054934f0701a2bb28ba86cfaac7e49d96
poohtime <pooh_time@naver.com>
2024-02-14 14:45:46 +0900

    html 코드 안먹히게, 글자수 제한, 폰트수정

--- public/js/sendMessage-modal.js
+++ public/js/sendMessage-modal.js
@@ -127,7 +127,7 @@ function createMessage(data, lastChild) {
         <div class="one-message">
             <div class="message-time">${day[1].substr(0, 5)}</div>
                 <div class="message-text-box">
-                    <div class="message-text">${data.message}</div>
+                    <div class="message-text">${data.message.replace(/</g, "&lt;").replace(/>/g, "&gt;")}</div>
                     </div>
                 </div>
             </div>
