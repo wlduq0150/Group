dc0b8ece3d067d89d76e037c70b14181772fdcdc
dnjs5432 <dnjs5432@naver.com>
2024-02-16 10:29:45 +0900

    feat:정말화가나

--- public/js/notice-modal.js
+++ public/js/notice-modal.js
@@ -1,32 +1,30 @@
-// notice-modal.js
 document.addEventListener("DOMContentLoaded", function () {
     const modal = document.getElementById("noticeModal");
     const closeBtn = document.querySelector(".close");
     const closeModalBtn = document.getElementById("closeModalBtn");
     const ignoreForeverCheckbox = document.getElementById("ignoreForever");
 
-    // Check local storage if the modal should be displayed
+    // 로컬 스토리지에서 모달 표시 여부를 확인
     const showModal = !localStorage.getItem("hideModalForever");
 
+    // 모달 보여야하면 보여주기
     if (showModal) {
         modal.style.display = "block";
     }
 
-    // Close modal event listener
+    // 모달 닫기
     closeBtn.addEventListener("click", closeModal);
     closeModalBtn.addEventListener("click", closeModal);
 
     function closeModal() {
-        modal.style.display = "none";
+        modal.style.display = "none"; //모달 숨기기
         if (ignoreForeverCheckbox.checked) {
-            localStorage.setItem("hideModalForever", true);
+            localStorage.setItem("hideModalForever", true); // 앞으로 보지 않기
         }
     }
 
-    // Click outside of modal to close
-    window.addEventListener("click", function (event) {
-        if (event.target === modal) {
-            closeModal();
-        }
-    });
+    // iframe 내부 문서에 접근하여 스타일 변경
+    const iframeDocument =
+        document.querySelector(".modal-iframe").contentWindow.document;
+    iframeDocument.body.style.zoom = "200%";
 });
